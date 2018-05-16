b3e306749655368991cf8e6708f745b96e1164c6
Dhiyaunikitty <39320187+Dhiyaunikitty@users.noreply.github.com>
2018-05-15 18:05:12 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+// // window.alert("welcome");
+// console.log("hiiiiiiiiiiiii");
+// console.log("5*2");
+// console.log(5*2);
+// console.log(19*37);
+// console.log("5*2="+5*2);
+// console.log("37+19 =", 37+19)
+// console.log("37-19 =",37-19)
+// console.log("37/19 =",37/19)
+// console.log("37%19 =",37%19)
+// console.log("10%3 =",10%3)
+window.setTimeout(answer(), 20000)
+
+function answer() {
+    document.getElementById("math").innerHTML = "answer";
+    document.getElementById("question").innerHTML = 37-19;
+}
+    
